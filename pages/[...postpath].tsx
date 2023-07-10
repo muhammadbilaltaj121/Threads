0295b5cf0da0fe47f962dd302a76d33e51538f04
Muhammadbilal Taj <60062343+muhammadbilaltaj121@users.noreply.github.com>
2023-07-11 00:05:41 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://aiousolvedassignment.com/graphql"
+	const endpoint = "https://iaapk.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://aiousolvedassignment.com/' + encodeURI(path as string)
+					'https://iaapk.com/' + encodeURI(path as string)
 				}`,
 			},
 		};
